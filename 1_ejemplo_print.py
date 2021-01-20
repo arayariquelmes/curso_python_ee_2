ff71d1a9994200433155cf055f02f3f16e0fcf59
Sebastián Araya <arayariquelmes@gmail.com>
2021-01-20 00:16:50 -0300

    Agregar ejemplos de print e input

---
+++ 1_ejemplo_print.py
@@ -0,0 +1,11 @@
+#Esto representa ejemplos de la funcion print
+print("Hola Mundo desde Fundamentos", sep=" ", end="\n")
+print("Hola",2021)
+print("Esto se va a hacer despues del primero", end=" ")
+print("Esto se hace al final")
+print(24.0,60,sep=";")
+
+print("el 2021 volveremos a  la \"normalidad\"")
+print("el 2021 volveremos a la 'normalidad'")
+print('el 2021 volveremos a la "normalidad"')
+print('el 2021 volveremos a la \'normalidad\'')
---
+++ 3_ejemplo_input.py
@@ -0,0 +1,8 @@
+print("Ingrese nombre:")
+nombre = input()
+#Agregar la lectura de la edad
+print("Ingrese edad:")
+edad = int(input())
+edad10 = edad + 10
+print("Hola",nombre,"Mucho Gusto! tu edad en 10 años es"
+,edad10)
